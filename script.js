065178e97504d89feea30b1e9c7ed83460f5e73a
Natalie-Cardot <nataliecardot@gmail.com>
2021-01-24 14:24:27 -0800

    Scramble list items

--- script.js
+++ script.js
@@ -23,13 +23,21 @@ createList();
 
 // Insert list items into DOM
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement('li');
+  // Copy array with spread syntax
+  [...richestPeople]
+    // Map over array items to create an array of objects, one for each item, which also includes a random number to sort with
+    .map((a) => ({ value: a, num: Math.random() }))
+    // Sort numerical ascending by random number
+    .sort((a, b) => a.num - b.num)
+    // Turn (numerically sorted according to randomly generated numbers) array back into an array of strings (with only names)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li');
 
-    // data- precedes custom attribute in HTML5
-    listItem.setAttribute('data-index', index);
+      // data- precedes custom attribute in HTML5
+      listItem.setAttribute('data-index', index);
 
-    listItem.innerHTML = `
+      listItem.innerHTML = `
       <span class="number">${index + 1}</span>
       <div class="draggable" draggable="true">
         <p class="person-name">${person}</p>
@@ -37,8 +45,8 @@ function createList() {
       </div>
     `;
 
-    listItems.push(listItem);
+      listItems.push(listItem);
 
-    draggable_list.appendChild(listItem);
-  });
+      draggable_list.appendChild(listItem);
+    });
 }
